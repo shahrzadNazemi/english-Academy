3724f1018a9ea79b4462ffe7b575cf7fa61330c3
Sharhzad <shahrzadnazemi.sn@gmail.com>
2018-12-13 10:25:27 +0330

    d

--- util/config.js
+++ util/config.js
@@ -1,14 +1,14 @@
 module.exports.databaseServer = 'http://localhost:9090'
 
-module.exports.uploadPathVideo = "C:/Users/Ardeshir/IdeaProjects/fileAccess/site/upload/video";
+module.exports.uploadPathVideo = "/var/fileAccess/site/upload/video";
 
-module.exports.uploadPathSound = "C:/Users/Ardeshir/IdeaProjects/fileAccess/site/upload/sound";
+module.exports.uploadPathSound = "/var/fileAccess/site/upload/sound";
 
 module.exports.downloadPathVideo = "http://localhost:9000/upload/video";
 
 module.exports.downloadPathSound = "http://localhost:9000/upload/sound";
 
-module.exports.uploadPathStuImage = "C:/Users/Ardeshir/IdeaProjects/fileAccess/site/upload/student/avatar"
+module.exports.uploadPathStuImage = "/var/fileAccess/site/upload/student/avatar"
 
 module.exports.downloadPathStuImage= "http://localhost:9000/upload/student/avatar";
 
