315d4ba13294327ddd279cc4482151d4fab2c8ef
Sharhzad <shahrzadnazemi.sn@gmail.com>
2019-02-18 12:44:05 +0330

    d

--- routes/statistic.js
+++ routes/statistic.js
@@ -74,25 +74,61 @@ router.get('/', (req, res)=> {
 
 })
 
+router.get('/ticket/:supId', (req, res)=> {
+    let statistic = {};
+    database.getTicketBySupId(req.params.supId, (data)=> {
+        if (data == -1) {
+            response.InternalServer('مشکلی در سرور پیش آمده است.لطفا دوباره تلاش کنید.', {}, (result)=> {
+                res.json(result)
+            })
+        }
+        else {
+            let close = []
+            let answered = []
+            let open = []
+            statistic.total = data.length
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].status == "closed") {
+                    close.push (data[i])
+                }
+                if (data[i].status == "answered") {
+                    answered.push (data[i])
+                }
+                if (data[i].status == "open") {
+                    open.push(data[i])
+                }
+            }
+            statistic.closeCount = close.length
+            statistic.openCount = open.length
+            statistic.answeredCount = answered.length
+
+            response.response('اطلاعات آماری', statistic, (result)=> {
+                res.json(result)
+            })
+        }
+    })
+
+})
+
 module.exports = router
 
-module.exports.calculateProgress=(lsnId , cb)=>{
-    database.getAllLessons((lessons)=>{
-        if(lessons == 0){
+module.exports.calculateProgress = (lsnId, cb)=> {
+    database.getAllLessons((lessons)=> {
+        if (lessons == 0) {
             cb(0)
         }
-        else if(lessons == -1){
+        else if (lessons == -1) {
             cb(-1)
         }
-        else{
-            let k= 0
-            for(var i=0;i<lessons.length;i++){
-                if(lessons[i]._id == lsnId){
+        else {
+            let k = 0
+            for (var i = 0; i < lessons.length; i++) {
+                if (lessons[i]._id == lsnId) {
                     k = i
                 }
             }
-            let progress = (k+1)/lessons.length
-            console.log("progresssssss",progress)
+            let progress = (k + 1) / lessons.length
+            console.log("progresssssss", progress)
             cb(progress)
         }
     })
