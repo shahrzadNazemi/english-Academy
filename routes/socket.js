9389a3fa614955a69e62794748ae489c0643fadb
Sharhzad <shahrzadnazemi.sn@gmail.com>
2019-02-25 11:03:00 +0330

    f

--- routes/socket.js
+++ routes/socket.js
@@ -99,7 +99,7 @@ io.sockets.on('connection', function (socket) {
                         chatRoom.endTime = parseInt(chatRoom.endTime)
                     }
 
-                    if (chatRoom.startTime <=  moment().format('HH') &&  moment().format('HH') <= chatRoom.endTime) {
+                    if (9 <=  moment().format('HH') &&  moment().format('HH') <= 12) {
                         if (user.chatAdmin) {
                             database.getChatAdminById(user._id, (chatAdmin)=> {
                                 database.studentByChId(user.chatroom._id, (result)=> {
