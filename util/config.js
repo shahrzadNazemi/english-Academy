36aa64cb8e0a2af4a462f278136080edf9c52204
Sharhzad <shahrzadnazemi.sn@gmail.com>
2019-05-13 10:45:07 +0430

    d

--- util/config.js
+++ util/config.js
@@ -8,81 +8,81 @@ module.exports.uploadPathSound = "/var/fileAccess/site/upload/sound";
 
 module.exports.uploadPathQvoice = "/var/fileAccess/site/upload/question";
 
-module.exports.downloadPathQvoice = "http://185.94.99.69:9000/upload/question";
+module.exports.downloadPathQvoice = "https://academi.rahasahamrah.com/upload/question";
 
-module.exports.downloadPathVideo = "http://185.94.99.69:9000/upload/video";
+module.exports.downloadPathVideo = "https://academi.rahasahamrah.com/upload/video";
 
 module.exports.uploadPathSupporterImage = "/var/fileAccess/site/upload/supporter/avatar"
 
 module.exports.uploadPathTicketImg = "/var/fileAccess/site/upload/ticket/image"
 
-module.exports.downloadPathTicketImg = "http://185.94.99.69:9000/upload/ticket/image";
+module.exports.downloadPathTicketImg = "https://academi.rahasahamrah.com/upload/ticket/image";
 
-module.exports.downloadPathSupporterImage = "http://185.94.99.69:9000/upload/supporter/avatar";
+module.exports.downloadPathSupporterImage = "https://academi.rahasahamrah.com/upload/supporter/avatar";
 
-module.exports.downloadPathSound = "http://185.94.99.69:9000/upload/sound";
+module.exports.downloadPathSound = "https://academi.rahasahamrah.com/upload/sound";
 
-module.exports.downloadPathTrick = "http://185.94.99.69:9000/upload/trick";
+module.exports.downloadPathTrick = "https://academi.rahasahamrah.com/upload/trick";
 
 module.exports.uploadPathStuImage = "/var/fileAccess/site/upload/student/avatar";
 
 module.exports.uploadPathNotifImage = "/var/fileAccess/site/upload/notification/avatar";
 
-module.exports.downloadPathNotifImage = "http://185.94.99.69:9000/upload/notification/avatar";
+module.exports.downloadPathNotifImage = "https://academi.rahasahamrah.com/upload/notification/avatar";
 
-module.exports.downloadPathStuImage = "http://185.94.99.69:9000/upload/student/avatar";
+module.exports.downloadPathStuImage = "https://academi.rahasahamrah.com/upload/student/avatar";
 
 module.exports.uploadPathLevelImage = "/var/fileAccess/site/upload/level/avatar";
 
-module.exports.downloadPathLevelImage = "http://185.94.99.69:9000/upload/level/avatar";
+module.exports.downloadPathLevelImage = "https://academi.rahasahamrah.com/upload/level/avatar";
 
 module.exports.uploadPathLessonImage = "/var/fileAccess/site/upload/lesson/avatar";
 
-module.exports.downloadPathLessonImage = "http://185.94.99.69:9000/upload/lesson/avatar";
+module.exports.downloadPathLessonImage = "https://academi.rahasahamrah.com/upload/lesson/avatar";
 
-module.exports.downloadPathExamImage = "http://185.94.99.69:9000/upload/exam/avatar";
+module.exports.downloadPathExamImage = "https://academi.rahasahamrah.com/upload/exam/avatar";
 
 module.exports.uploadPathExamImage = "/var/fileAccess/site/upload/exam/avatar";
 
 module.exports.uploadPathIDCard = "/var/fileAccess/site/upload/student/IDCard";
 
-module.exports.downloadPathPathIDCard = "http://185.94.99.69:9000/upload/student/IDCard";
+module.exports.downloadPathPathIDCard = "https://academi.rahasahamrah.com/upload/student/IDCard";
 
-module.exports.downloadPathPersonalImg= "http://185.94.99.69:9000/upload/student/personalImg";
+module.exports.downloadPathPersonalImg= "https://academi.rahasahamrah.com/upload/student/personalImg";
 
 module.exports.uploadPathPersonalImg = "/var/fileAccess/site/upload/student/personalImg";
 
 module.exports.uploadPathChatAdminImage = "/var/fileAccess/site/upload/chatAdmin/avatar"
 
-module.exports.downloadPathChatAdminImage= "http://185.94.99.69:9000/upload/chatAdmin/avatar";
+module.exports.downloadPathChatAdminImage= "https://academi.rahasahamrah.com/upload/chatAdmin/avatar";
 
-module.exports.downloadPathChatroomImage= "http://185.94.99.69:9000/upload/chatroom/avatar";
+module.exports.downloadPathChatroomImage= "https://academi.rahasahamrah.com/upload/chatroom/avatar";
 
 module.exports.uploadPathChatroomImage = "/var/fileAccess/site/upload/chatroom/avatar"
 
 module.exports.uploadPathVoiceMsg = "/var/fileAccess/site/upload/message"
 
-module.exports.downloadPathVoiceMsg= "http://185.94.99.69:9000/upload/message";
+module.exports.downloadPathVoiceMsg= "https://academi.rahasahamrah.com/upload/message";
 
-module.exports.downloadPathVIPVoiceMsg= "http://185.94.99.69:9000/upload/message/vip/voice";
+module.exports.downloadPathVIPVoiceMsg= "https://academi.rahasahamrah.com/upload/message/vip/voice";
 
 module.exports.uploadPathVIPVoiceMsg = "/var/fileAccess/site/upload/message/vip/voice"
 
 module.exports.uploadPathTutorImage= "/var/fileAccess/site/upload/tutor/avatar"
 
-module.exports.downloadPathTutorImage= "http://185.94.99.69:9000/upload/tutor/avatar";
+module.exports.downloadPathTutorImage= "https://academi.rahasahamrah.com/upload/tutor/avatar";
 
-module.exports.downloadPathVIPImgMsg= "http://185.94.99.69:9000/upload/message/vip/img";
+module.exports.downloadPathVIPImgMsg= "https://academi.rahasahamrah.com/upload/message/vip/img";
 
 module.exports.uploadPathVIPImgMsg = "/var/fileAccess/site/upload/message/vip/img"
 
 module.exports.uploadPathVipFile = "/var/fileAccess/site/upload/vip/file"
 
-module.exports.downloadPathVipFile = "http://185.94.99.69:9000/upload/vip/file"
+module.exports.downloadPathVipFile = "https://academi.rahasahamrah.com/upload/vip/file"
 
 module.exports.uploadPathCpAvatar = "/var/fileAccess/site/upload/cp/avatar"
 
-module.exports.downloadPathCpAvatar = "http://185.94.99.69:9000/upload/cp/avatar"
+module.exports.downloadPathCpAvatar = "https://academi.rahasahamrah.com/upload/cp/avatar"
 
 
 
